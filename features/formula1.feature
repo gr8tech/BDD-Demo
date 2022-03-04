476f492e16810af1018eee3dd6aee2a925a05d35
moosabonomali <gr8tech01@gr8tech.onmicrosoft.com>
2022-03-04 20:47:15 +0200

    Ammended the scenario according to requirements. Testing uses Safari on Mac, otherwise uses Chrome. Put chrome driver in path

--- features/formula1.feature
+++ features/formula1.feature
@@ -1,7 +1,5 @@
-@fixture.driver.safari
 Feature: Testing formula1.com website
-
   Scenario: All teams listed
-     Given driver has launched
-      When we browse to teams url
-      Then available teams are "{Alfa Romeo,AlphaTauri,Alpine,Aston Martin,Ferrari,Haas F1 Team,McLaren,Mercedes,Red Bull Racing,Williams}"
+     Given user navigates to "formula1.com"
+      When user hovers over the "Teams"
+      Then all the 10 Teams are listed
--- features/steps/test.py
+++ features/steps/test.py
@@ -1,31 +1,39 @@
 from behave import given, when, then, fixture, use_fixture
 
-from selenium.webdriver import Safari
+from selenium.webdriver import Safari, ActionChains
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 
+import platform
 
-@given(u'driver has launched')
+
+@given('user navigates to "formula1.com"')
 def step_impl(context):
-    pass
-
-
-@when(u'we browse to teams url')
-def step_impl(context):
-    driver = Safari()
-    found_teams = []
-    driver.get('https://www.formula1.com/en/teams.html')
-    driver.implicitly_wait(10)
-    trust = driver.find_element(by=By.CSS_SELECTOR, value='#truste-consent-button')
+    if platform.system() == 'Darwin':
+        context.driver = Safari()
+    else:
+        context.driver = Chrome()  # put chrome driver in the path
+    context.driver.get('https://www.formula1.com')
+    context.driver.maximize_window()
+    trust = context.driver.find_element(by=By.CSS_SELECTOR, value='#truste-consent-button')
     if trust:
         trust.click()
-    teams = driver.find_elements(by=By.CSS_SELECTOR, value='.listing-info .name ')
-    for team in teams:
-        found_teams.append(team.text.strip())
-    context.teams = ','.join(sorted(found_teams))
-    driver.quit()
+    assert 'F1' in context.driver.title
 
-@then(
-    u'available teams are "{teams}"')
-def step_impl(context, teams):
-    assert(context.teams, teams)
+
+@when('user hovers over the "Teams"')
+def step_impl(context):
+    team_link = context.driver.find_element(by=By.XPATH, value='//*[@id="primaryNav"]/div/div[2]/ul/li[6]/a/span')
+    ActionChains(context.driver).move_to_element(team_link).perform()
+    for y in range(10):
+        ActionChains(context.driver).move_by_offset(1, y).perform()
+
+
+@then('all the 10 Teams are listed')
+def step_impl(context):
+    teams = context.driver.find_elements(by=By.XPATH,
+                                         value='//div[@class="nav-list teams"]//span[@class="name f1--xs"]')
+    for team in teams:
+        assert team.get_attribute("innerText") in ['Alfa Romeo', 'AlphaTauri', 'Alpine', 'Aston Martin', 'Ferrari',
+                                                   'Haas', 'McLaren', 'Mercedes', 'Red Bull', 'Williams']
+    context.driver.quit()
